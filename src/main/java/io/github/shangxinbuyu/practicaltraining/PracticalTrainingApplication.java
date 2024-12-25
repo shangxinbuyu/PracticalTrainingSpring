d435f3cecfb500635cf7616f39cd1c249ccf0d1d
shangxinbuyu <lx496957@gmail.com>
2024-12-25 18:24:08 +0800

    项目初始化

---
+++ src/main/java/io/github/shangxinbuyu/practicaltraining/PracticalTrainingApplication.java
@@ -0,0 +1,13 @@
+package io.github.shangxinbuyu.practicaltraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalTrainingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/shangxinbuyu/practicaltraining/PracticalTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shangxinbuyu.practicaltraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
